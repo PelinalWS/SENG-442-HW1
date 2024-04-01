73bd77b046d7757c778603759b6bb02d035e5684
PelinalWS <batucan971@gmail.com>
2024-04-01 22:07:30 +0300

    changed the matrix to keeping track of the visited ones

--- threaded_det.c
+++ threaded_det.c
@@ -3,15 +3,15 @@
 #include <pthread.h>
 #include <time.h>
 
-#define MAX_SIZE 16
-#define NUM_THREADS 4
+#define MAX_SIZE 4
+#define NUM_THREADS 1
 
-// Structure to pass parameters to threads
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 struct ThreadData {
     int* matrix;    // Pointer to the matrix
-    int n;          // Size of the matrix
     int column;     // Row to expand along
-    int sign;       // Sign multiplier
+    int* visited;
     int result;     // Partial result (determinant)
 };
 
@@ -19,7 +19,7 @@ void writeMatrix();
 int* readMatrix(char* filename);
 int laplaceDet(int* matrix, int n);
 void* calcDet(void* arg);
-int calcRec(int *matrix, int row, int column, int tid);
+int calcRec(int *matrix, int row, int column, int* visited);
 
 // Main function
 int main() {
@@ -75,9 +75,8 @@ int laplaceDet(int* matrix, int n) {
     for(int i = 0; i < NUM_THREADS; i++) {
         thread_data[i] = malloc(sizeof(struct ThreadData));
         thread_data[i]->matrix = matrix;
-        thread_data[i]->n = n;
         thread_data[i]->column = i * (n / NUM_THREADS); // Divide columns evenly among threads
-        thread_data[i]->sign = 1;
+        thread_data[i]->visited = malloc(MAX_SIZE * sizeof(int));
         pthread_create(&tid[i], NULL, calcDet, thread_data[i]);
     }
 
@@ -92,65 +91,49 @@ int laplaceDet(int* matrix, int n) {
 void* calcDet(void* arg) {
     struct ThreadData* data = (struct ThreadData*)arg;
     int* matrix = data->matrix;
-    int n = data->n;
     int column = data->column;
-    int sign = data->sign;
-
+    int *visited = data-> visited;
     // Store the partial result in the data structure
 
     for(int i = 0; i < MAX_SIZE/NUM_THREADS; i++) {
+        printf("0, %d : %d\n", column+i, matrix[i]);
         if(i % 2 == 0){
-            data->result += calcRec(matrix, 0, column + i, column);
+            data->result += calcRec(matrix, 0, column + i, visited);
         } else {
-            data->result -= calcRec(matrix, 0, column + i, column);
+            data->result -= calcRec(matrix, 0, column + i, visited);
         }
     }
     pthread_exit(NULL);
 }
 
-/*
-        0 1 0 0
-        0 0 0 0  (1,0)
-        0 0 0 0
-        0 0 0 0
-
-*/
-int calcRec(int *matrix, int row, int column, int tid){ //(x,y) of the matrix element that's determinant is being calculated is (column, row)
+int calcRec(int *matrix, int row, int column, int* visited){ //(x,y) of the matrix element that's determinant is being calculated is (column, row)
     int edge_size = MAX_SIZE - row;                             //since the matrix is square shaped, top-left corner will always be (row, row)
     long determinant = 0;                                //the column will dictate what index not to take
+    visited[row] = column;
     // Base case: determinant of 1x1 matrix is the element itself
     if (edge_size == 1) {
-        //printf("tid = %d: n = %d: index = 0: m = %d\n", tid, edge_size, matrix[0]);
-        return matrix[0];
+        printf("n = %d: index = 0: m = %d\n", edge_size, matrix[row * MAX_SIZE + column]);
+        return matrix[row * MAX_SIZE + column];
     } else {
-        int* temp = malloc((edge_size - 1) * (edge_size - 1) * sizeof(int));
-        if (temp == NULL) {
-            perror("Memory allocation failed");
-            exit(EXIT_FAILURE);
-        }
-        for (int oldRow = 1, newRow = 0; oldRow < edge_size; oldRow++, newRow++) {
-            for (int oldColumn = 0, newColumn = 0; oldColumn < edge_size; oldColumn++) {
-                if (oldColumn != column) {
-                    temp[newRow * (edge_size - 1) + newColumn] = matrix[oldRow * edge_size + oldColumn];
-                    newColumn++;
+        for(int i = 0, k = 0; i < MAX_SIZE; i++){
+            int f = 1;
+            for(int j = 0; j <= row; j++){
+                if(visited[j] == i){
+                    f = 0;
                 }
             }
-        }
-
-        for(int a = 0; a < (edge_size - 1); a++){
-            for(int b = 0; b < edge_size -1; b++){
-                //printf("tid = %d: n = %d: index = %d: m = %d\n", tid, edge_size, a*(edge_size - 1)+b, temp[a*(edge_size - 1)+b]);
+            if(f == 1){
+                printf("K = %d, %d, %d : %d\n", k, row+1, i, matrix[(row+1)*MAX_SIZE +i]);
+                int cofactor = calcRec(matrix, row + 1, i, visited);
+                if(k % 2 == 0){
+                    determinant += matrix[row * MAX_SIZE + i] * cofactor;
+                } else {
+                    determinant -= matrix[row * MAX_SIZE + i] * cofactor;
+                }
+                k++;
             }
         }
-        //printf("\n");
-        for(int i = 0; i < edge_size - 1; i++){
-            if(i % 2 == 0) {
-                determinant += calcRec(temp, row + 1, i, tid);
-            } else {
-                determinant -= calcRec(temp, row + 1, i, tid);
-            }
-        }
-        free(temp);
     }
+    visited[row] = -1;
     return determinant;
 }
